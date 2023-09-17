7189448b25752a0ded950aec4853222401f7381e
ThomasSavary08 <savarythomas2102@gmail.com>
2023-09-17 23:34:18 +0200

    Setup file

--- setup.py
+++ setup.py
@@ -6,21 +6,9 @@ setup(
     author='Thomas Savary',
     author_email='savarythomas2102@gmail.com',
     description='A python package to compute Lyapunov exponents, covariant Lyapunov vectors (CLV) and adjoints of a dynamical system.',
-    long_description=open('README.md').read(),  # Lisez le contenu du fichier README.md
-    long_description_content_type='text/markdown',
-    url='https://lien_vers_votre_projet',
-    packages=find_packages(where='src'),  # Spécifiez le répertoire source
-    package_dir={'': 'src'},  # Répertoire racine des packages
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-    ],
-    keywords='votre, mots-clés, ici',
-    install_requires=[
-        # Liste des dépendances requises
-    ],
+    url='https://github.com/ThomasSavary08/Lyapynov',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    keywords='Lyapunov, Lyapunov exponents, LCE, Covariant Lyapunov vectors, CLV, Dynamical systems, ODE',
+    install_requires=['numpy'],
 )
